07ad590c47ddee19ce2a3561c646b3f7f79b5eb8
Jim Nasr <jnacr13@jwsj13.local>
2018-08-30 16:20:37 -0400

    wikiproject 0.6

---
+++ wiki06/resources/story.js
@@ -0,0 +1,1411 @@
+var story = {
+"pages": [
+{
+"image": "demo_start.png",
+"image2x": "demo_start@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Demo-start",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    621,
+    627,
+    1300,
+    687
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    621,
+    627,
+    1300,
+    687
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    621,
+    557,
+    1300,
+    617
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    621,
+    557,
+    1300,
+    617
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    621,
+    697,
+    1300,
+    757
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    621,
+    697,
+    1300,
+    757
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    621,
+    767,
+    1300,
+    827
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    621,
+    767,
+    1300,
+    827
+   ],
+   "page": 35
+  }
+]}
+,{
+"image": "anon_startpage.png",
+"image2x": "anon_startpage@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon_startpage",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    668,
+    551,
+    1252,
+    595
+   ],
+   "page": 6
+  }
+,{
+  "rect": [
+    668,
+    551,
+    1252,
+    595
+   ],
+   "page": 6
+  }
+]}
+,{
+"image": "user_startpage.png",
+"image2x": "user_startpage@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user_startpage",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    668,
+    551,
+    1252,
+    595
+   ],
+   "page": 25
+  }
+,{
+  "rect": [
+    668,
+    551,
+    1252,
+    595
+   ],
+   "page": 25
+  }
+]}
+,{
+"image": "user_signup.png",
+"image2x": "user_signup@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user_signup",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    740,
+    144,
+    1181,
+    926
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    740,
+    144,
+    1181,
+    926
+   ],
+   "page": 2
+  }
+]}
+,{
+"image": "admin_pharma_signup.png",
+"image2x": "admin_pharma_signup@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "admin-pharma_signup",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    740,
+    144,
+    1181,
+    639
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    740,
+    144,
+    1181,
+    639
+   ],
+   "page": 5
+  }
+]}
+,{
+"image": "admin_pharma_signup_copy.png",
+"image2x": "admin_pharma_signup_copy@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "admin-pharma_signup copy",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    740,
+    144,
+    1181,
+    639
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    740,
+    144,
+    1181,
+    639
+   ],
+   "page": 18
+  }
+]}
+,{
+"image": "anon_search_dropdown.png",
+"image2x": "anon_search_dropdown@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-search-dropdown",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    668,
+    551,
+    1252,
+    811
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    668,
+    551,
+    1252,
+    811
+   ],
+   "page": 7
+  }
+]}
+,{
+"image": "anon_doc.png",
+"image2x": "anon_doc@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-doc",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    1507,
+    320,
+    1830,
+    460
+   ],
+   "page": 26
+  }
+,{
+  "rect": [
+    1507,
+    320,
+    1830,
+    460
+   ],
+   "page": 26
+  }
+]}
+,{
+"image": "pharma_doc_edit.png",
+"image2x": "pharma_doc_edit@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-doc-edit",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    112,
+    216,
+    351,
+    256
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    112,
+    216,
+    351,
+    256
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "pharma_doc_new.png",
+"image2x": "pharma_doc_new@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-doc-new",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 10
+  }
+,{
+  "rect": [
+    645,
+    144,
+    761,
+    184
+   ],
+   "page": 23
+  }
+,{
+  "rect": [
+    645,
+    144,
+    761,
+    184
+   ],
+   "page": 23
+  }
+]}
+,{
+"image": "pharma_doc_new_states.png",
+"image2x": "pharma_doc_new_states@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-doc-new-states",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    503,
+    144,
+    625,
+    184
+   ],
+   "page": 11
+  }
+,{
+  "rect": [
+    645,
+    144,
+    761,
+    184
+   ],
+   "page": 23
+  }
+,{
+  "rect": [
+    645,
+    144,
+    761,
+    184
+   ],
+   "page": 23
+  }
+]}
+,{
+"image": "pharma_doc_published.png",
+"image2x": "pharma_doc_published@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-doc-published",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    387,
+    144,
+    478,
+    184
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    387,
+    144,
+    478,
+    184
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    498,
+    144,
+    614,
+    184
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    498,
+    144,
+    614,
+    184
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    112,
+    216,
+    351,
+    256
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    112,
+    216,
+    351,
+    256
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "pharma_doc_published_image.png",
+"image2x": "pharma_doc_published_image@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-doc-published-image",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "anon_image.png",
+"image2x": "anon_image@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-image",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "asset_error.png",
+"image2x": "asset_error@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "asset-error",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    482,
+    339,
+    1110,
+    734
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    482,
+    339,
+    1110,
+    734
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "pharma_dashboard.png",
+"image2x": "pharma_dashboard@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-dashboard",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    276,
+    517,
+    336,
+    533
+   ],
+   "page": 22
+  }
+,{
+  "rect": [
+    276,
+    517,
+    336,
+    533
+   ],
+   "page": 22
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    979,
+    288,
+    1347,
+    569
+   ],
+   "page": 12
+  }
+,{
+  "rect": [
+    979,
+    288,
+    1347,
+    569
+   ],
+   "page": 12
+  }
+,{
+  "rect": [
+    1387,
+    649,
+    1755,
+    930
+   ],
+   "page": 9
+  }
+,{
+  "rect": [
+    1387,
+    649,
+    1755,
+    930
+   ],
+   "page": 9
+  }
+,{
+  "rect": [
+    301,
+    144,
+    459,
+    184
+   ],
+   "page": 23
+  }
+,{
+  "rect": [
+    301,
+    144,
+    459,
+    184
+   ],
+   "page": 23
+  }
+]}
+,{
+"image": "anon_pharma_dashboard.png",
+"image2x": "anon_pharma_dashboard@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-pharma-dashboard",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    979,
+    288,
+    1347,
+    569
+   ],
+   "page": 13
+  }
+,{
+  "rect": [
+    979,
+    288,
+    1347,
+    569
+   ],
+   "page": 13
+  }
+]}
+,{
+"image": "user_pharma_dashboard.png",
+"image2x": "user_pharma_dashboard@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-pharma-dashboard",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 25
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 25
+  }
+]}
+,{
+"image": "pharma_projects.png",
+"image2x": "pharma_projects@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-projects",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    144,
+    270,
+    184
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    112,
+    144,
+    270,
+    184
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    1388,
+    649,
+    1756,
+    930
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    1388,
+    649,
+    1756,
+    930
+   ],
+   "page": 20
+  }
+,{
+  "rect": [
+    290,
+    144,
+    421,
+    184
+   ],
+   "page": 19
+  }
+,{
+  "rect": [
+    290,
+    144,
+    421,
+    184
+   ],
+   "page": 19
+  }
+]}
+,{
+"image": "pharma_projects_archive.png",
+"image2x": "pharma_projects_archive@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-projects-archive",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    164,
+    288,
+    532,
+    569
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    112,
+    144,
+    236,
+    184
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    112,
+    144,
+    236,
+    184
+   ],
+   "page": 18
+  }
+]}
+,{
+"image": "pharma_projects_copy.png",
+"image2x": "pharma_projects_copy@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-projects copy",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    863,
+    610,
+    1057,
+    650
+   ],
+   "page": 21
+  }
+,{
+  "rect": [
+    863,
+    610,
+    1057,
+    650
+   ],
+   "page": 21
+  }
+]}
+,{
+"image": "pharma_projects_copy_2.png",
+"image2x": "pharma_projects_copy_2@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-projects copy 2",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1140,
+    200,
+    1164,
+    224
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    1140,
+    200,
+    1164,
+    224
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    863,
+    610,
+    1057,
+    650
+   ],
+   "page": 18
+  }
+,{
+  "rect": [
+    863,
+    610,
+    1057,
+    650
+   ],
+   "page": 18
+  }
+]}
+,{
+"image": "pharma_dashboard_modal.png",
+"image2x": "pharma_dashboard_modal@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "pharma-dashboard-modal",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    940,
+    637,
+    1057,
+    677
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    940,
+    637,
+    1057,
+    677
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    1089,
+    637,
+    1206,
+    677
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    1089,
+    637,
+    1206,
+    677
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "pharma_dash_upload.png",
+"image2x": "pharma_dash_upload@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-Dash Upload",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    329,
+    119,
+    1595,
+    914
+   ],
+   "page": 24
+  }
+,{
+  "rect": [
+    329,
+    119,
+    1595,
+    914
+   ],
+   "page": 24
+  }
+]}
+,{
+"image": "pharma_dash_upload_sucess.png",
+"image2x": "pharma_dash_upload_sucess@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "Pharma-Dash Upload sucess",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    329,
+    119,
+    1595,
+    914
+   ],
+   "page": 15
+  }
+,{
+  "rect": [
+    329,
+    119,
+    1595,
+    914
+   ],
+   "page": 15
+  }
+]}
+,{
+"image": "user_doc.png",
+"image2x": "user_doc@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1509,
+    406,
+    1832,
+    546
+   ],
+   "page": 27
+  }
+,{
+  "rect": [
+    1509,
+    406,
+    1832,
+    546
+   ],
+   "page": 27
+  }
+]}
+,{
+"image": "anon_doc_comments.png",
+"image2x": "anon_doc_comments@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-doc comments",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    112,
+    144,
+    243,
+    184
+   ],
+   "page": 34
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "user_doc_comments.png",
+"image2x": "user_doc_comments@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc-comments",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1518,
+    455,
+    1808,
+    967
+   ],
+   "page": 30
+  }
+,{
+  "rect": [
+    1518,
+    455,
+    1808,
+    967
+   ],
+   "page": 30
+  }
+]}
+,{
+"image": "user_doc_comments_edit.png",
+"image2x": "user_doc_comments_edit@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc-comments-edit",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1708,
+    564,
+    1789,
+    624
+   ],
+   "page": 30
+  }
+,{
+  "rect": [
+    1708,
+    564,
+    1789,
+    624
+   ],
+   "page": 30
+  }
+]}
+,{
+"image": "admin_doc_comments_edit.png",
+"image2x": "admin_doc_comments_edit@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "admin-doc-comments-edit",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    288,
+    1479,
+    1132
+   ],
+   "page": 35
+  }
+,{
+  "rect": [
+    112,
+    288,
+    1479,
+    1132
+   ],
+   "page": 35
+  }
+]}
+,{
+"image": "user_doc_comments_edit.png",
+"image2x": "user_doc_comments_edit@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc-comments-edit",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1708,
+    564,
+    1789,
+    624
+   ],
+   "page": 32
+  }
+,{
+  "rect": [
+    1708,
+    564,
+    1789,
+    624
+   ],
+   "page": 32
+  }
+]}
+,{
+"image": "user_doc_comments_edit_copy.png",
+"image2x": "user_doc_comments_edit_copy@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc-comments-edit copy",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    387,
+    144,
+    478,
+    184
+   ],
+   "page": 8
+  }
+,{
+  "rect": [
+    387,
+    144,
+    478,
+    184
+   ],
+   "page": 8
+  }
+]}
+,{
+"image": "user_doc_comments_delete.png",
+"image2x": "user_doc_comments_delete@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "user-doc-comments-delete",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    662,
+    371,
+    1259,
+    710
+   ],
+   "page": 31
+  }
+,{
+  "rect": [
+    662,
+    371,
+    1259,
+    710
+   ],
+   "page": 31
+  }
+]}
+,{
+"image": "admin_comments.png",
+"image2x": "admin_comments@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "admin-comments",
+'type': 'regular',
+"links": [
+]}
+,{
+"image": "anon_doc_history.png",
+"image2x": "anon_doc_history@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "anon-doc-history",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    112,
+    144,
+    259,
+    184
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    112,
+    144,
+    259,
+    184
+   ],
+   "page": 7
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    0,
+    0,
+    1920,
+    64
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "admin_doc_history_copy.png",
+"image2x": "admin_doc_history_copy@2x.png",
+"width": 1920,
+"height": 1080,
+"title": "admin-doc-history copy",
+'type': 'regular',
+"links": [
+{
+  "rect": [
+    1694,
+    351,
+    1763,
+    367
+   ],
+   "page": 30
+  }
+,{
+  "rect": [
+    1694,
+    351,
+    1763,
+    367
+   ],
+   "page": 30
+  }
+]}
+   ]
+,"resolutions": [2],
+"title": "WikiApp_Mocks_Demo",
+"highlightLinks": false
+}
